895617dbbe374ad7376c937317d0ec60fa376f6a
Nick Arvelo <narvelo@users.noreply.github.com>
2017-09-30 19:51:07 -0400

    Add files via upload

---
+++ rack-unit.js
@@ -0,0 +1,10 @@
+// Rack unit extension
+// Author: Nicholas Arvelo
+
+numi.addUnit({
+    "id": "rack unit",
+    "phrases": "rack unit, rack units, rack, racks, server rack, RU, U",
+    "baseUnitId": "m",
+    "format" : "U",
+    "ratio" : 0.04445,
+});
